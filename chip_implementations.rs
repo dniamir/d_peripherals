92b19249bbc9b79504139684704ba20d00122d7f
Darien Niamir <darien.niamir@gmail.com>
2025-12-29 21:25:28 -0800

    Update chip implementations to allow for a delay and reset/ignore if nRF I2C signal is not returned

--- chip_implementations.rs
+++ chip_implementations.rs
@@ -1,6 +1,8 @@
 use embassy_sync::mutex::Mutex;
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
-use embassy_nrf::twim::{Twim};
+use embassy_time::{Timer, Duration, with_timeout};
+use embassy_nrf::twim::{Twim, Error as TwimError};
+use core::future::Future;
 
 use crate::d_peripherals::chip::CommProvider;
 use crate::{d_log::dlogger::DLogger, d_info};  // Logging
@@ -23,14 +25,74 @@ impl From<TwimError> for CommError {
         CommError::TwimError(err)
     }
 }
+
+// Async function to add a delay to a command
+async fn add_timeout<Fut>(op: Fut, timeout_ms: u64, recovery_delay_ms: u64, return_ok_on_timeout: bool) -> Result<(), CommError>
+where
+    Fut: Future<Output = Result<(), CommError>>,
+{
+    DLogger::hold();
+    
+    let timeout_func = with_timeout(
+        Duration::from_millis(timeout_ms),
+        op,
+    ).await;
+
+    let result = match timeout_func {
+        Ok(comm_result) => {
+            if comm_result.is_err() { 
+                d_info!("Comm Error");
+            } 
+            else { 
+                d_info!("Comm Success");
+            }
+        }
+        Err(_) => {
+            d_info!("Comm timeout");
+            Timer::after_millis(recovery_delay_ms).await;
+        }
+    };
+
+    DLogger::release();
+    Ok(())
+}
+
+
+// write_read and write for nRF52840
+impl CommProvider for I2CMutexWrapper {
+    async fn write_read(&self, i2c_address: u8, reg: u8, reg_vals: &mut [u8]) -> Result<(), CommError> {
+
+        // Get TWIM from MUTEX
         let mut twim = self.0.lock().await;
-        twim.write_read(i2c_address, &[reg], reg_vals).await?;
-        Ok(())
+
+        // Define communication without calling it
+        let reg_buf: [u8; 1] = [reg];
+        let com = twim.write_read(i2c_address, &reg_buf, reg_vals);
+
+        // Call communication with a timeout
+        add_timeout(
+            async { Ok(com.await?) }, 
+            200, 
+            200,
+            false,
+        ).await
     }
 
-    async fn write(&self, i2c_address: u8, reg: u8, reg_val: u8) -> Result<(), I2CError> {
+    async fn write(&self, i2c_address: u8, reg: u8, reg_val: u8) -> Result<(), CommError> {
+        
+        // Get TWIM from MUTEX
         let mut twim = self.0.lock().await;
-        twim.write(i2c_address, &[reg, reg_val]).await?;
-        Ok(())
+
+        // Define communication without calling it
+        let reg_buf: [u8; 2] = [reg, reg_val];
+        let com = twim.write(i2c_address, &reg_buf);
+        
+        // Call communication with a timeout
+        add_timeout(
+            async { Ok(com.await?) }, 
+            200, 
+            200,
+            true,
+        ).await                      
     }
 }
