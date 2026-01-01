5af25d13fcdc752ff20cd71bddb2bac0d94ecb0c
Darien Niamir <darien.niamir@gmail.com>
2025-12-31 16:28:16 -0800

    Add ability to use shadow register maps in place of actual comm communication

--- chip_implementations.rs
+++ chip_implementations.rs
@@ -1,3 +1,4 @@
+use core::cell::RefCell;
 use embassy_sync::mutex::Mutex;
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_time::{Timer, Duration, with_timeout};
@@ -105,5 +106,114 @@ impl CommProvider for NRFI2CMutex {
             200, 
             200,
         ).await                      
+    }
+}
+
+
+/// Struct for communicating with a shadow map instead of actual communication
+/// eg all reads/writes can be done with a shadow register map, and then later
+/// done with i2cmutex
+
+pub struct ShadowComm<COMM> {
+    
+    pub provider: COMM,                         // A normal comm provider, like the I2C mutex
+    pub shadow_registers: RefCell<[u8; 256]>,   // The shadow register map
+    dirty_bits: RefCell<[u8; 256]>,             // Tracks registers that have been updated
+}
+
+impl<COMM> ShadowComm<COMM> {
+
+    /// Checks if a specific register has been modified 
+    pub fn is_dirty(&self, reg: u8) -> bool {
+        let reg_idx = reg as usize;
+        let byte = self.dirty_bits.borrow()[reg_idx / 8];
+        (byte & (1 << (reg_idx % 8))) != 0
+    }
+
+    // Resets the entire tracking back to clean
+    pub fn clear_dirty(&self) {
+        self.dirty_bits.borrow_mut().fill(0);
+    }
+
+    /// Resets the shadow register map to 0
+    pub fn reset_shadow(&self) {
+        // 1. Set all register values to 0
+        self.shadow_registers.borrow_mut().fill(0);
+        
+        // 2. Mark everything as "Clean"
+        self.clear_dirty();
+    }
+
+}
+
+impl<COMM> Addressable for ShadowComm<COMM>
+where 
+    COMM: Addressable
+{
+    fn set_address(&mut self, address: u8) {
+        self.provider.set_address(address);
+    }
+}
+
+impl<Comm> ShadowComm<Comm> 
+where 
+    Comm: CommProvider
+{
+    pub fn new(comm: Comm) -> Self {
+        Self {
+            provider: comm,
+            shadow_registers: RefCell::new([0u8; 256]),
+            dirty_bits: RefCell::new([0u8; 256]),
+        }
+    }
+
+    /// Explicitly sync a specific register
+    pub async fn true_raw_write(&self, reg: u8, reg_val: u8) -> Result<(), CommError> {
+        self.provider.write(reg, reg_val).await
+    }
+
+    /// Write all the shadow register values to the device
+    /// Only writes dirty registers
+    pub async fn sync_all(&self) -> Result<(), CommError> {
+        
+        let shadow = self.shadow_registers.borrow();
+        
+        // Iterate directly through the array
+        for (reg, &reg_val) in shadow.iter().enumerate() {
+
+            // Only write dirty registers
+            if !self.is_dirty(reg as u8) { continue; }
+
+            // Attempt the hardware write
+            self.true_raw_write(reg as u8, reg_val).await?;
+        }
+
+        Ok(())
+    }
+}
+
+impl<COMM> CommProvider for ShadowComm<COMM> {
+
+    // Write and read to and from the shadow register field map
+    async fn write_read(&self, reg: u8, reg_vals: &mut [u8]) -> Result<(), CommError> {
+        let shadow = self.shadow_registers.borrow();
+        let start = reg as usize;
+        let end = start + reg_vals.len();
+        reg_vals.copy_from_slice(&shadow[start..end]);
+        Ok(())
+    }
+
+    // Write to shadow register field map
+    async fn write(&self, reg: u8, reg_val: u8) -> Result<(), CommError> {
+
+        let reg_idx = reg as usize;
+
+        // Update shadow register value
+        self.shadow_registers.borrow_mut()[reg_idx] = reg_val;
+
+        // Mark as dirty (1 = Dirty, 0 = Clean)
+        self.dirty_bits.borrow_mut()[reg_idx] = 1;
+
+        Ok(())
     }
 }
